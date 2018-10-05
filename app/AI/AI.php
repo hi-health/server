fda898df68cf5563a4a43f9a8e11fdba7180e797
YuHsueh Wu on Macbook Pro <r05548020@ntu.edu.tw>
2018-10-05 18:02:12 +0800

    AI v0.2

--- app/AI/AI.php
+++ app/AI/AI.php
@@ -4,6 +4,7 @@ namespace App\AI;
 
 use Exception;
 use App\ServicePlanVideo;
+use Log;
 
 abstract class AI
 {
@@ -25,10 +26,11 @@ abstract class AI
 
     public function __construct($template, $test, $param)
     {
-        $this->templateData = $this->parseTemplateData($template);
-        $this->testData = $this->parseTestData($test);
+        
         $this->session = $param['session'];
         $this->test_repeat_time = $param['repeat_time'];
+        $this->templateData = $this->parseTemplateData($template);
+        $this->testData = $this->parseTestData($test);
         $this->template_repeat_time = count($this->templateData['acc_x']);
         
         $this->major_threshold = $param['major_threshold'];
@@ -43,34 +45,122 @@ abstract class AI
         for ($x = 0; $x < count($series); $x++) {
             $a = 0;
             foreach ($series as $key => $value) {
-                $a += $value * $series[$key+$x];
-            }
+                $a += $value * (isset($series[$key+$x]) ? $key+$x : 0);
+            } 
             $A[] = $a;
         } 
         
         return $A;
     }
 
+    protected function findMaxIndexOfAutocorrelation($arr, $t1, $t2)
+    {   
+        $t2_1 = ceil($t2/2);
+        $t2_2 = $t2 - $t2_1;
+        $max_index_arr = [];
+        Log::alert(strval($t1));
+        Log::alert(strval($t2));
+        Log::alert(strval(count($arr)));
+        for ($i=1; $i < $this->test_repeat_time; $i++) { 
+            $max_index_arr[] = array_search(max(array_slice($arr, $i*$t1-$t2_1+1, $t2)), array_slice($arr, $i*$t1-$t2_1+1, $t2)) + $i*$t1-$t2_1+1;
+        }
+        return $max_index_arr;
+        
+    }
+
+    protected function seperateEachRepeat($arr, $index)
+    {
+        $arr_seperated = [];
+        $arr_seperated[] = array_slice($arr,0,$index[0]);
+        for ($i=1; $i < count($index); $i++) { 
+            $arr_seperated[] = array_slice($arr,$index[$i-1],$index[$i]-$index[$i-1]);
+        }
+        $arr_seperated[] = array_slice($arr,$index[count($index)-1]);
+
+        return $arr_seperated;
+    }
+
     protected function parseTestData($test)
     {
         $testData = [];
-        foreach ($test as $key1 => $session) {
-            foreach ($session as $key2 => $repeat) {
-                foreach ($repeat as $key3 => $sample) {
+        $testData_eachRepeat = [];
 
-                    $testData[$key1]['acc_x'][$key2][$key3] = round(floatval($sample[0]),5);
-                    $testData[$key1]['acc_y'][$key2][$key3] = round(floatval($sample[1]),5);
-                    $testData[$key1]['acc_z'][$key2][$key3] = round(floatval($sample[2]),5);
-                    $testData[$key1]['roll'][$key2][$key3] = round(floatval($sample[3]),5);
-                    $testData[$key1]['yaw'][$key2][$key3] = round(floatval($sample[4]),5);
-                    $testData[$key1]['pitch'][$key2][$key3] = round(floatval($sample[5]),5);
-                    $testData[$key1]['rot_x'][$key2][$key3] = round(floatval($sample[6]),5);
-                    $testData[$key1]['rot_y'][$key2][$key3] = round(floatval($sample[7]),5);
-                    $testData[$key1]['rot_z'][$key2][$key3] = round(floatval($sample[8]),5);
-                }
+        foreach ($test as $key1 => $session) {
+            Log::alert('session: '.strval(count($session)));
+            Log::alert('session: '.strval(count($session[0])));
+            Log::alert('session: '.strval(count($session[0][0])));
+            $t1 = round(count($session[0])/$this->test_repeat_time);
+            $t2 = round(count($session[0])/$this->test_repeat_time *2/3);
+            foreach ($session[0] as $key2 => $sample) {
+
+                $testData[$key1]['acc_x'][$key2] = round(floatval($sample[0]),5);
+                $testData[$key1]['acc_y'][$key2] = round(floatval($sample[1]),5);
+                $testData[$key1]['acc_z'][$key2] = round(floatval($sample[2]),5);
+                $testData[$key1]['roll'][$key2] = round(floatval($sample[3]),5);
+                $testData[$key1]['yaw'][$key2] = round(floatval($sample[4]),5);
+                $testData[$key1]['pitch'][$key2] = round(floatval($sample[5]),5);
+                $testData[$key1]['rot_x'][$key2] = round(floatval($sample[6]),5);
+                $testData[$key1]['rot_y'][$key2] = round(floatval($sample[7]),5);
+                $testData[$key1]['rot_z'][$key2] = round(floatval($sample[8]),5);
             }
+            $testData_eachRepeat[$key1]['acc_x'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['acc_x'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['acc_x']), $t1, $t2
+                                            )
+                );
+
+            $testData_eachRepeat[$key1]['acc_y'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['acc_y'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['acc_y']), $t1, $t2
+                                            )
+                );
+            $testData_eachRepeat[$key1]['acc_z'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['acc_z'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['acc_z']), $t1, $t2
+                                            )
+                );
+            $testData_eachRepeat[$key1]['roll'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['roll'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['roll']), $t1, $t2
+                                            )
+                );
+            $testData_eachRepeat[$key1]['yaw'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['yaw'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['yaw']), $t1, $t2
+                                            )
+                );
+            $testData_eachRepeat[$key1]['pitch'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['pitch'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['pitch']), $t1, $t2
+                                            )
+                );
+            $testData_eachRepeat[$key1]['rot_x'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['rot_x'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['rot_x']), $t1, $t2
+                                            )
+                );
+            $testData_eachRepeat[$key1]['rot_y'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['rot_y'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['rot_y']), $t1, $t2
+                                            )
+                );
+            $testData_eachRepeat[$key1]['rot_z'] = 
+                $this->seperateEachRepeat(  $testData[$key1]['rot_z'], 
+                                            $this->findMaxIndexOfAutocorrelation(
+                                                $this->autocorrelation($testData[$key1]['rot_z']), $t1, $t2
+                                            )
+                );
+            
         }
-        return $testData;
+        return $testData_eachRepeat;
     }
 
     protected function parseTemplateData($template)

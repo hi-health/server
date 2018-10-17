9093f0ffaa408931b5c656ee957d16226f72591a
bocheng <bocheng4o7@gmail.com>
2018-10-17 11:56:28 +0800

    點數轉移頁面 新增輸入帳號和密碼的輸入框

--- resources/views/points/point_transfer.blade.php
+++ resources/views/points/point_transfer.blade.php
@@ -32,6 +32,10 @@
     <table align="left">
       <form action="{{ url('api/point/'.$users_id.'/transfer') }}" method="post">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <font face="monospace" size="4">帳號 : </font>
+        <input type="text" name="account" class="form-control" placeholder="請輸入您的帳號"><br><br>
+        <font face="monospace" size="4">密碼 : </font>
+        <input type="text" name="password" class="form-control" placeholder="請輸入您的密碼"><br><br>
         <font face="monospace" size="4">轉移給 : </font>
         <input type="text" name="receiver_account" class="form-control" placeholder="請輸入轉入者之手機號碼"><br><br>
         <font face="monospace" size="4">欲轉移的點數 : </font>

dab2687ecea861646aecfdbc7818090203804ed5
YuHsueh Wu on Macbook Pro <r05548020@ntu.edu.tw>
2018-10-24 17:57:24 +0800

    Update resources/views/points/point_transfer.blade.php

--- resources/views/points/point_transfer.blade.php
+++ resources/views/points/point_transfer.blade.php
@@ -40,47 +40,10 @@
 		</div>
 
 	</form>
-  
-<<<<<<< HEAD
-  <div class="w3-padding-32">
-    <div class="w3-bar w3-border">
-      <a href="{{ route('point-list-all-transaction', ['users_id' => $users_id]) }}" class="w3-bar-item w3-button">交易紀錄</a>
-      <a href="{{ route('point-list-consume', ['users_id' => $users_id]) }}" class="w3-bar-item w3-button">使用紀錄</a>
-      <a href="{{ route('point-list-produce', ['users_id' => $users_id]) }}" class="w3-bar-item w3-button">獲得紀錄</a>
-      <a href="{{ route('point-transfer', ['users_id' => $users_id]) }}" class="w3-bar-item w3-button">點數轉移</a>
-    </div><br><br>
-    <table align="left">
-        <!-- @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul style="color:#fff;">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-            </ul>
-        </div>
-        @endif -->  <!-- 需要web middleware -->
-      <form action="{{ url('api/point/'.$users_id.'/transfer') }}" method="post">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <font face="monospace" size="4">密碼 : </font>
-        <input type="password" name="password" class="form-control" placeholder="請輸入您的密碼"><br><br>
-        <font face="monospace" size="4">轉移給 : </font>
-        <input type="text" name="receiver_account" class="form-control" placeholder="請輸入轉入者之手機號碼"><br><br>
-        <font face="monospace" size="4">欲轉移的點數 : </font>
-        <input type="text" name="transferred_point" class="form-control" placeholder="請輸入欲轉出點數" style="ime-mode:disabled" onkeyup="return ValidateNumber(this,value)"><br><br>
-        <input type="submit" onClick="return confirm('點數一旦轉移則無法收回，確定要轉移嗎？');" value="送出" class="button">
 
-      </form>
-    </table>
-  </div>
-</header>
-
-</body>
-</html>
-=======
 @endsection
 
 
->>>>>>> origin/test
 
 
 <script>

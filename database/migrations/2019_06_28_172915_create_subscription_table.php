e82216c08f1bffe5660346145f6586ceceadb12a
michael860828 <michael860828@gmail/com>
2019-06-28 20:31:58 +0800

    Create 2019_06_28_172915_create_subscription_table.php

---
+++ database/migrations/2019_06_28_172915_create_subscription_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * 
+     */
+    public function up()
+    {
+        Schema::create('subscription_table', function($table){
+            $table->increments('id');
+            $table->integer('service');
+            $table->integer('plan');
+            $table->date('due date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * 
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscription_table');
+    }
+}

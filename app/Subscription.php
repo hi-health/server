84eba661e9c8b3d794d6b561622dbe9e4146203f
michael860828 <michael860828@gmail/com>
2019-07-02 12:08:16 +0800

    Update Subscription.php

--- app/Subscription.php
+++ app/Subscription.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Subscription extends Model
 {
     use SoftDeletes,
-        HihealthDate;
+        HiHealthDate;
 
     public $table = 'subscription';
 

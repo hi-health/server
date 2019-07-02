cf853f40142c1b7447fc4474c66e716543f6ba4d
michael860828 <michael860828@gmail/com>
2019-07-02 15:47:07 +0800

    Create 2019_06_30_230211_rename_subscription_table.php

---
+++ database/migrations/2019_06_30_230211_rename_subscription_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameSubscriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * 
+     */
+    public function up()
+    {
+        Schema::table('subscription', function ($table) {
+            $table->dropColumn('due date');
+            $table->date('due_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * 
+     */
+    public function down()
+    {
+        Schema::table('subscription', function ($table) {
+            $table->dropColumn('due_date');
+            $table->date('due date');
+        });
+    }
+}
